42c22d1bba12f8c190f6ba5e8a5512d461bab9b5
NahirIsray24 <81720703+NahirIsray24@users.noreply.github.com>
2021-04-23 11:26:55 -0300

    Update TablaDispersa.java

--- src/main/java/TablaDispersa.java
+++ src/main/java/TablaDispersa.java
@@ -10,13 +10,15 @@
  */
 import java.util.*;
 public class TablaDispersa {
-    static final double R=0.618034;
+    static final double R=0.618034;//constante para la dispersion
     
     static final int TAMTABLA =101;
-    private int numElementos;
-    private double factorCarga;
+    
+    private int numElementos; //núm de elementos que hayan la tabla,incluyendo las bajas, se almacena en la variable 
+    private double factorCarga;// cuando se alcance el 0.5 se pueda generar un aviso
     private casaRclass[] tabla;
-    //inicializacion de la tabla
+    
+    //inicializacion: constructor de la tabla
     public TablaDispersa(){
         tabla= new casaRclass[TAMTABLA];
         int i;
@@ -33,7 +35,8 @@ public class TablaDispersa {
         d= transformarClav(clave);
         // aplica aritmética modular para obtener dirección base
         p = (int)(d%TAMTABLA);
-        while(tabla[p]!=null && tabla[p].getCodigo().equals(clave)){
+        // exploración cuadrática hasta encontrar una posición vacía (null)
+        while(tabla[p]!=null && tabla[p].getCodigo().equals(clave)){ //El método equals(),se utiliza para comparar dos objetos
             i++;
             p=p+i*i;
             p=p%TAMTABLA;
@@ -41,8 +44,8 @@ public class TablaDispersa {
         return p;
     }
      long transformarClav (String clave){
-        long d;
-        d=0;
+        long d; //v d almacena el valor entero
+        d=0; //0 min posicion de la tabla 
         int j;
         for(j=0;j<Math.min(clave.length(), 5);j++){
             d=d*27+(int)clave.charAt(j);
@@ -54,9 +57,10 @@ public class TablaDispersa {
            
     
 
-    //inserccion
+
     public void insertar(casaRclass r){
         int pos; 
+        // primero se busca la posición que debe ocupar, el método direccion()devuelve la posición buscada
         pos= direccion(r.getCodigo());
         tabla[pos]=r;
         numElementos++;
